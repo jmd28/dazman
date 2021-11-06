b3d3d7a7f523e21b746508077908a27d294a664c
jmd28 <jmd28@st-andrews.ac.uk>
2021-11-06 13:00:16 +0000

    add initial game screen

--- frontend/core/src/com/mygdx/game/Game.java
+++ frontend/core/src/com/mygdx/game/Game.java
@@ -2,30 +2,37 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Game extends ApplicationAdapter {
+public class Game extends com.badlogic.gdx.Game {
 	SpriteBatch batch;
 	Texture img;
-	
+	BitmapFont font;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+
+//		img = new Texture("badlogic.jpg");
+		this.setScreen(new GameScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+//		ScreenUtils.clear(1, 0, 0, 1);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+//		img.dispose();
+		font.dispose();
 	}
 }
