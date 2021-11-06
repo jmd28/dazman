b9c55806d223219c535a640cd737df2fe069687a
jmd28 <jmd28@st-andrews.ac.uk>
2021-11-06 16:20:04 +0000

    smol jon go in maze

--- frontend/core/src/com/mygdx/game/Ghost.java
+++ frontend/core/src/com/mygdx/game/Ghost.java
@@ -11,6 +11,7 @@ import java.util.Stack;
 
 public class Ghost extends GameObject{
     private final int SCALE = 100;
+    private final int VELOCITY = 5;
     private Direction dir; //direction of movement
 //    private Texture texture;
 
@@ -43,16 +44,16 @@ public class Ghost extends GameObject{
     public void setDirection(Direction dir) {
         if (dir == Direction.UP) {
 //            sprite.setRotation(90);
-            setPosition(x, y + SCALE);
+            setPosition(x, y + VELOCITY);
         } else if (dir == Direction.DOWN) {
 //            sprite.setRotation(-90);
-            setPosition(x, y - SCALE);
+            setPosition(x, y - VELOCITY);
         } else if (dir == Direction.LEFT) {
 //            sprite.setRotation(180);
-            setPosition(x - SCALE, y);
+            setPosition(x - VELOCITY, y);
         } else if (dir == Direction.RIGHT) {
 //            sprite.setRotation(0);
-            setPosition(x + SCALE, y);
+            setPosition(x + VELOCITY, y);
         }
     }
 
